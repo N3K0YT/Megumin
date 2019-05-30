c773a5f92278d89b9f5d9f47735cb71b4723e806
N3K0 <fazeneko123@gmail.com>
2019-05-29 20:33:35 -0400

    yeet

--- index.js
+++ index.js
@@ -12,10 +12,8 @@ const slaps = require('./assets/slaps.json')
 client.on('ready', () => {
     client.user.setActivity('your mom | m. ')
     console.log(`Logged in as: ${client.user.tag}!`)
-    const nani1 = client.emojis.find(emoji => emoji.name === "nani1");
-    const nani2 = client.emojis.find(emoji => emoji.name === "nani2");
-    console.log(nani1)
-    console.log(nani2)
+    const nani = client.emojis.get("583450493745889285")
+   
 })
 client.on('message', msg => {
 	if(msg.content === 'm.ping'){
@@ -57,7 +55,7 @@ client.on('message', msg => {
 		const values = Object.values(slaps)
 		const slap = values[parseInt(Math.random() * values.length)]
 		var user = msg.mentions.users.first()
-		const embed = new Discord.RichEmbed().setDescription(`<@${msg.author.id}> slapped <@${user.id}>! `).setImage(slap)
+		const embed = new Discord.RichEmbed().setDescription(`<@${msg.author.id}> slapped <@${user.id}>! ${nani.toString()} `).setImage(slap)
 		msg.channel.send(embed)
 		}
 })
