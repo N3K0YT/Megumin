3bf68191290973155b2524c9c37765bca0a5f47e
N3K0 <fazeneko123@gmail.com>
2019-05-30 17:24:35 -0400

    Management commands

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ const colors = require('./assets/colors.json')
 const dl = require('discord-leveling')
 
 /*							TODO
--Add XP system (not important)
--Add Profile (not important)
 -Add management commands
 -Add games
 ...
@@ -114,6 +112,69 @@ client.on('message', msg => {
 		const embed = new Discord.RichEmbed().setColor(color).setDescription(`<@${msg.author.id}> hugged <@${user.id}>!`).setImage(hug)
 		msg.channel.send(embed)
 		}
+})
+client.on('message', msg => {
+	if(msg.content.startsWith('m.ban ')
+	var user = msg.mentions.users.first
+	    if(!msg.member.hasPermission(["BAN_MEMBERS", "ADMINISTRATOR"])) return msg.channel.send("You don't have permission to perform this command!")
+
+    let bannedMember = await bot.fetchUser(args[0])
+        if(!bannedMember) return msg.channel.send("Please provide a user to ban!")
+
+    let reason = args.slice(1).join(" ")
+        if(!reason) reason = "No reason given!"
+
+    if(!msg.guild.me.hasPermission(["BAN_MEMBERS", "ADMINISTRATOR"])) return msg.channel.send("I dont have permission to perform this command!")|
+    msg.delete()
+    try {
+        //msg.guild.ban(bannedMember, {reason: reason})
+        msg.channel.send(`${bannedMember.tag} has been banned from the guild!`)
+    } catch(e) {
+        console.log(e.message)
+    }
+
+    let embed = new Discord.RichEmbed()
+    .setColor(colours.red_light)
+    .setAuthor(`${msg.guild.name} Modlogs`, msg.guild.iconURL)
+    .addField("Moderation:", "ban")
+    .addField("Moderated on:", `${bannedMember.username} (${bannedMember.id})`)
+    .addField("Moderator:", msg.author.username)
+    .addField("Reason:", reason)
+    .addField("Date:", msg.createdAt.toLocaleString())
+    
+        let logs = msg.guild.channels.find(c => c.name === "logs")
+        logs.send(embed)
+})
+client.on('message', msg => {
+	if(msg.content.startsWith('m.ban ')
+	    if(!msg.member.hasPermission(["BAN_MEMBERS", "ADMINISTRATOR"])) return msg.channel.send("You don't have permission to perform this command!")
+
+    let bannedMember = await bot.fetchUser(args[0])
+        if(!bannedMember) return msg.channel.send("Please provide a user ban!")
+
+    let reason = args.slice(1).join(" ")
+        if(!reason) reason = "No reason given!"
+
+    if(!msg.guild.me.hasPermission(["BAN_MEMBERS", "ADMINISTRATOR"])) return msg.channel.send("I dont have permission to perform this command!")|
+    msg.delete()
+    try {
+        msg.guild.unban(bannedMember, {reason: reason})
+        msg.channel.send(`${bannedMember.tag} has been unbanned from the guild!`)
+    } catch(e) {
+        console.log(e.message)
+    }
+
+    let embed = new Discord.RichEmbed()
+    .setColor(colours.red_light)
+    .setAuthor(`${msg.guild.name} Modlogs`, msg.guild.iconURL)
+    .addField("Moderation:", "kick")
+    .addField("Moderated on:", `${kickedMember.username} (${kickedMember.id})`)
+    .addField("Moderator:", msg.author.username)
+    .addField("Reason:", reason)
+    .addField("Date:", msg.createdAt.toLocaleString())
+    
+        let logs = msg.guild.channels.find(c => c.name === "logs")
+        logs.send(embed)
 })
 
 client.login(process.env.token)
