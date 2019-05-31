7da5733963338d4139f026f3f88b420d0ee90b16
N3K0 <fazeneko123@gmail.com>
2019-05-30 21:15:36 -0400

    Avatar

--- index.js
+++ index.js
@@ -276,10 +276,10 @@ client.on('message', msg => {
 		var secs = client.uptime / 1000;
 		var days = Math.floor(secs / 86400)
 		var mins = Math.floor(secs /3600)
-		var s = secs % 60
-		var seconds = s.split('.')
-		console.log(seconds[0])
-		const embed = new Discord.RichEmbed().setColor(color).setTitle('Megumin Info').addField('Made by:', 'Aqua_').addField('Uptime:', `${days} days / ${mins} minutes / ${seconds.toFixed(2)} seconds`).setFooter(`Requested by ${msg.author.username}`, msg.author.avatarURL)
+		var seconds = secs % 60
+		console.log(seconds.toFixed(2)
+		var seconds = seconds.toFixed(2)
+		const embed = new Discord.RichEmbed().setColor(color).setTitle('Megumin Info').addField('Made by:', 'Aqua_').addField('Uptime:', `${days} days / ${mins} minutes / ${seconds} seconds`).setFooter(`Requested by ${msg.author.username}`, msg.author.avatarURL)
 		msg.channel.send(embed)
         }
 })
