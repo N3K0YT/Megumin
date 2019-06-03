ba74a90878c1514c89841086ad62ad732f2222bd
N3K0 <fazeneko123@gmail.com>
2019-06-03 17:51:40 -0400

    Command handler, xp and cash

--- index.js
+++ index.js
@@ -12,7 +12,10 @@ client.on('message', async (msg)=>{
 let randomxp = Math.floor(Math.random() * 30) + 1;
 	const gId = msg.guild.id
 	const aId = msg.author.id
+	const hasxp = await dl.Fetch(aId)
+	if (!hasxp.xp) {
 	var addxp = await dl.AddXp(aId, randomxp)
+}
 	var xp = await addxp.newxp
 	let profile = dl.Fetch(aId)
 	var level = await profile.level
