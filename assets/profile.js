3acd2762159fb508acf054547028a2c04d61309a
N3K0 <fazeneko123@gmail.com>
2019-06-06 10:49:37 -0400

    Lel

---
+++ assets/profile.js
@@ -0,0 +1,4 @@
+exports.run = (client, member, jimp, colors){
+	let welcomebg = jimp.read('./assets/welcomebg.png')
+	let avatar = jimp.read('member.avatarURL')
+}
--- index.js
+++ index.js
@@ -2,15 +2,15 @@ const Discord = require('discord.js')
 const client = new Discord.Client()
 const embed = new Discord.RichEmbed()
 const prefix = 'm.'
+const jimp = require('jimp')
 const didDaily = new Set();
-const mysql = require('mysql')
 const colors = require('./assets/colors.json')
 const owner = '383749208575967244'
+const profileCard = require('./assets/profile.js')
 
 client.on('guildMemberAdd', member => {
-	con.query(`INSERT INTO xplist (userId, xp) VALUES (${member.id}, 0)`, function (err) => {
-		if (err) throw err
-		console.log(`Successfully added ${member.username} to database`)
+	profileCard.run(client, member,jimp, colors)
+		
 })
 client.on('message', msg =>{
 	// IGNORE BOTS
@@ -31,30 +31,6 @@ client.on('ready', () => {
 	client.user.setActivity('with explosive magic | m. ')
 	console.log(`Logged in as ${client.user.tag}!`)
 })
-// DB CONNECTION
-con = mysql.createConnection({
-			host: 'remotemysql.com',
-			user: 'mzBFUTIoNt',
-			port:  '3306',
-			password: 'WmmCoYMmQc',
-			database: 'mzBFUTIoNt'
-})
-con.connect( function (err) => {
-	if (err) throw err;
-	console.log('Sucessfully connected to db')
-}
 // XP SYSTEM
-client.on('message', async (msg) => {
-	if (msg.content.startsWith(prefix)) return;
-	if (msg.author.bot) return;
-	let randomxp = Math.floor(Math.random() * 30) + 
-	con.query(`SELECT * FROM xplist WHERE userId = ${msg.author.id}`, (err, results) => {
-		if (err) throw err;
-		if (results[0].length === 0) {
-		con.query(`INSERT INTO xplist (userId, xp) VALUES ('${msg.author.id}', randomxp )`)
-})
-}
-con.query(`UPDATE xplist SET xp = ${results[0].xp} + ${randomxp} WHERE userId = ${msg.author.id}`)
-})
 // LOGIN
 client.login(process.env.token)
