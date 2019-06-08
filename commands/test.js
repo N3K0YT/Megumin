82f0fef3b5dd7117812a4a0317d08775df78e36b
N3K0YT <fazeneko123@gmail.com>
2019-06-07 23:06:38 -0400

    test

--- commands/test.js
+++ commands/test.js
@@ -1,7 +1,7 @@
 const Jimp = require('jimp')
 exports.run = (client, msg) => {
 	bg = Jimp.read('profile.png')
-	avatar = jimp.read(msg.author.avatar)
+	avatar = jimp.read(`${msg.author.avatarURL}`)
 	mask = Jimp.read('mask.png')
 	avatar.mask(mask, 0, 0).write("avatar-circle.png");
 }
