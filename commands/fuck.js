05b3247ca8817d11791056a52778980e05209236
N3K0YT <fazeneko123@gmail.com>
2019-06-07 21:53:27 -0400

    More actions

--- commands/fuck.js
+++ commands/fuck.js
@@ -1,17 +1,13 @@
 const Discord = require('discord.js')
-const fucks = require('../assets/fucks.json')
+const oofs = require('../assets/oofs.json')
 const colors = require('../assets/colors.json')
 exports.run = (client, msg, args) => {
   if (!msg.channel.nsfw) return msg.reply('no.')
-	    const values = Object.values(fucks)
-		const fuck = values[parseInt(Math.random() * values.length)]
+	    const values = Object.values(oofs)
+		const kill = values[parseInt(Math.random() * values.length)]
 		const randcol = Object.values(colors)
 		const color = randcol[parseInt(Math.random() * randcol.length)]
 		var user = msg.mentions.users.first()
-		const embed = new Discord.RichEmbed().setColor(color).setDescription(`<@${msg.author.id}> fucked <@${user.id}>!`).setImage(fuck)
+		const embed = new Discord.RichEmbed().setColor(color).setDescription(`<@${msg.author.id}> killed <@${user.id}>!`).setImage(kill)
 		msg.channel.send(embed)
-		if (user.tag === 'Megumin#6443'){
-			const embed = new Discord.RichEmbed().setTitle('O/\/\/)O').setColor(color)
-			msg.channel.send(embed)
-    }
 }
