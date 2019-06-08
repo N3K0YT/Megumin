f2ca2cc892fea9dbdf16e44307771357181c1118
N3K0YT <fazeneko123@gmail.com>
2019-06-08 12:14:51 -0400

    My brain in glitching

--- commands/joke.js
+++ commands/joke.js
@@ -1,7 +1,7 @@
 const api = require('superagent')
 exports.run =(client, msg) => {
 	api
-	.get('https://icanhazdadjoke.com/api')
+	.get('https://icanhazdadjoke.com')
 	.set('Accept', 'application/json')
 	.then(res => {
 		msg.channel.send(res.body.joke)
