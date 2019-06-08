5ba137a82cafb818c9d2c3a6368bfa3821e9c135
N3K0YT <fazeneko123@gmail.com>
2019-06-08 12:08:50 -0400

    More jokes :D

--- commands/joke.js
+++ commands/joke.js
@@ -1,8 +1,10 @@
 const api = require('superagent')
 exports.run =(client, msg) => {
-	api.get('https://icanhazdadjoke.com/api')
+	api
+	.get('https://icanhazdadjoke.com/api')
+	.set('Accept', 'application/json')
 	.then(res => {
-		msg.channel.send(res.joke)
+		msg.channel.send(res.body.joke)
 		
 })
 }
